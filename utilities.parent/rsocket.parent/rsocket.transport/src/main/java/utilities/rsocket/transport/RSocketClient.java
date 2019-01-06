e9c7dbd532ba1a8a2fcbc62c284fa91477883934
kakusuke-maruno <kakusuke.maruno@gmail.com>
2019-01-07 01:56:27 +0900

    change processor that made by Reactor

--- utilities.parent/rsocket.parent/rsocket.transport/src/main/java/utilities/rsocket/transport/RSocketClient.java
+++ utilities.parent/rsocket.parent/rsocket.transport/src/main/java/utilities/rsocket/transport/RSocketClient.java
@@ -7,15 +7,10 @@ import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
 
-import org.reactivestreams.Processor;
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
-
-import io.rsocket.Payload;
 import io.rsocket.RSocket;
 import io.rsocket.RSocketFactory;
+import io.rsocket.internal.UnboundedProcessor;
 import io.rsocket.transport.netty.client.TcpClientTransport;
 import io.rsocket.util.DefaultPayload;
 
@@ -23,27 +18,26 @@ public class RSocketClient {
 	private final RSocket rsocket;
 	private final AtomicLong seq = new AtomicLong();
 	private final ConcurrentMap<Long, FutureHolder> waiting = new ConcurrentHashMap<>();
-	private final PublishProcessor<FutureHolder, Payload> processor;
+	private final UnboundedProcessor<FutureHolder> processor = new UnboundedProcessor<>();
 
 	public RSocketClient(InetSocketAddress address) {
 		rsocket = RSocketFactory.connect().transport(TcpClientTransport.create(address)).start().block();
-		processor = new PublishProcessor<>(holder -> {
-			waiting.put(holder.id, holder);
-			int pos = 0;
-			byte[] meta = new byte[Long.BYTES + (holder.meta != null ? holder.meta.length : 0)];
-			Bits.putLong(meta, pos, holder.id.longValue());
-			pos += Long.BYTES;
-			if (holder.meta != null && holder.meta.length > 0) {
-				System.arraycopy(holder.meta, 0, meta, pos, holder.meta.length);
-			}
-			return DefaultPayload.create(holder.msg, meta);
-		});
 
 		final CountDownLatch startup = new CountDownLatch(1);
 		new Thread(() -> {
 			startup.countDown();
 			try {
-				rsocket.requestChannel(processor).map(payload -> {
+				rsocket.requestChannel(processor.map(holder -> {
+					waiting.put(holder.id, holder);
+					int pos = 0;
+					byte[] meta = new byte[Long.BYTES + (holder.meta != null ? holder.meta.length : 0)];
+					Bits.putLong(meta, pos, holder.id.longValue());
+					pos += Long.BYTES;
+					if (holder.meta != null && holder.meta.length > 0) {
+						System.arraycopy(holder.meta, 0, meta, pos, holder.meta.length);
+					}
+					return DefaultPayload.create(holder.msg, meta);
+				})).map(payload -> {
 					byte[] meta = payload.getMetadata().array();
 					Long id = Bits.getLong(meta, 0);
 					byte[] data = payload.getData().array();
@@ -93,54 +87,6 @@ public class RSocketClient {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-		}
-	}
-
-	private static class PublishProcessor<T, R> implements Processor<T, R> {
-		Subscriber<? super R> downstream;
-		Function<T, R> function;
-		CountDownLatch latch = new CountDownLatch(1);
-
-		public PublishProcessor(Function<T, R> function) {
-			this.function = function;
-		}
-
-		@Override
-		public void onSubscribe(Subscription s) {
-		}
-
-		@Override
-		public void onNext(T t) {
-			try {
-				latch.await();
-			} catch (InterruptedException e) {
-			}
-			R r = function.apply(t);
-			downstream.onNext(r);
-		}
-
-		@Override
-		public void onError(Throwable t) {
-			try {
-				latch.await();
-			} catch (InterruptedException e) {
-			}
-			downstream.onError(t);
-		}
-
-		@Override
-		public void onComplete() {
-			try {
-				latch.await();
-			} catch (InterruptedException e) {
-			}
-			downstream.onComplete();
-		}
-
-		@Override
-		public void subscribe(Subscriber<? super R> s) {
-			this.downstream = s;
-			latch.countDown();
 		}
 	}
 
